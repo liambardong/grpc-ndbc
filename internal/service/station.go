f7614bb2554029e28af5356acd5fa596d4fe82e7
Liam Bardong <liambardong@gmail.com>
2024-11-10 17:09:25 -0500

    Added changes to parse, get the list of stations from the database

--- internal/service/station.go
+++ internal/service/station.go
@@ -1,3 +1,38 @@
-package main
+package service
 
-import ()
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	pb "github.com/liambardong/grpc-ndbc/api/proto/v1"
+	"github.com/liambardong/grpc-ndbc/config"
+	"github.com/liambardong/grpc-ndbc/pkg/parsers"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+type StationService struct {
+	pb.UnimplementedStationServiceServer
+}
+
+func NewStationService() *StationService {
+	return &StationService{}
+}
+
+func (s *StationService) ListStation(ctx context.Context, _ *emptypb.Empty) (*pb.ListStationsResponse, error) {
+	url := fmt.Sprintf("%sstations/station_table.txt", config.NDBC_URL)
+
+	// Send a GET request
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error fetching the URL:", err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	stationsList, err := parsers.ParseStationTableResponse(resp)
+
+	for _, station := range stationsList {
+
+	}
+}
--- pkg/parsers/stations_table.go
+++ pkg/parsers/stations_table.go
@@ -5,23 +5,37 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+
+	"github.com/liambardong/grpc-ndbc/pkg/types"
 )
 
-
-func parseResponse(response *http.Response) ([]){
-	
+func ParseStationTableResponse(response *http.Response) ([]types.Station, error) {
+	stationList := []types.Station{}
 	scanner := bufio.NewScanner(response.Body)
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		if strings.HasPrefix(line,"#"){
+		if strings.HasPrefix(line, "#") {
 			continue
 		}
-		
+
 		fmt.Println(line)
-		
+
 		columns := strings.Split(line, "|")
 
-		
+		stationList = append(stationList, types.Station{
+			StationId: columns[0],
+			Owner:     columns[1],
+			Type:      columns[2],
+			Hull:      columns[3],
+			Name:      columns[4],
+			Payload:   columns[5],
+			TimeZone:  columns[6],
+			Forecast:  columns[7],
+			Note:      columns[8],
+		})
+
 	}
+
+	return stationList, nil
 }
